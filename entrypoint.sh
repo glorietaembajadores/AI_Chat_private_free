e15371d85ef54af93b615caa302f877e68445981
Pionono <glorietaembajadores@yahoo.es>
2025-06-26 14:29:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,17 @@ ollama serve &
 
 
 # Download some models
-echo "LLAMA Tiny...    1Gb....................................................."
+echo "Llama 3 8B          6Gb....................................................."
+ollama pull llama3:8b
+echo "LLAMA Tiny...       1Gb....................................................."
 ollama pull tinyllama
-echo "Gemma 2B.......    1Gb..................................................."
+echo "Gemma 2B            1Gb..................................................."
 ollama pull gemma:2b
-echo " Mistral  size 4Gb ..............................................."
+echo " Mistral            4Gb ..............................................."
 ollama pull mistral
-echo " LLAMA2 ............... 4Gb ......................................"
+echo " LLAMA2             4Gb ......................................"
 ollama pull llama2:7b
-echo " Phi-2 ............... 1Gb ......................................"
+echo " Phi-2              1Gb ......................................"
 ollama pull phi
 
 # Wait 3 minutes to download models
