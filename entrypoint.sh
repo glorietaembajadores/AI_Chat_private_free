e0da7c722fd4defb5634990ba737cfd8f6ee8f1d
Pionono <glorietaembajadores@yahoo.es>
2025-06-26 14:22:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-# Inicia el servidor Ollama en background
 ollama serve &
 
-# Espera a que el puerto 11434 esté listo
 
-# Descarga automáticamente el modelo más ligero
+
+# Download some models
 echo "LLAMA Tiny...    1Gb....................................................."
 ollama pull tinyllama
 echo "Gemma 2B.......    1Gb..................................................."
@@ -18,12 +17,8 @@ ollama pull llama2:7b
 echo " Phi-2 ............... 1Gb ......................................"
 ollama pull phi
 
+# Wait 3 minutes to download models
+sleep 180
 
-sleep 120
 
-# Puedes añadir más modelos si deseas:
-# ollama pull phi3
-# ollama pull llama2:7b
-
-# Continua con el server principal en primer plano para mantener el contenedor vivo
 fg %1
